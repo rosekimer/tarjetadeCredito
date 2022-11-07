ac666403102379a066b2ba66d1987af5fcf07a9d
Sar <samuel.alonso@people-media.com.mx>
2022-11-07 11:55:46 -0600

    background: rgb(63,94,251);bbackground: rgb(63,94,251);i mi orimer commit background: radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%);ackground: rgb(63,94,251); background: radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%); background: radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%);

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrearTarjetaComponent } from './components/crear-tarjeta/crear-tarjeta.component';
+import { ListaTarjetaComponent } from './components/lista-tarjeta/lista-tarjeta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearTarjetaComponent,
+    ListaTarjetaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
